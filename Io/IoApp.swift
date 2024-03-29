dc5cba1db15a0b4259f3a24ac85f6ea30d395c71
Carson Gross <carsongro@gmail.com>
2024-03-28 19:59:45 -0600

    Initial Commit

---
+++ Io/IoApp.swift
@@ -0,0 +1,17 @@
+//
+//  IoApp.swift
+//  Io
+//
+//  Created by Carson Gross on 3/28/24.
+//
+
+import SwiftUI
+
+@main
+struct IoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
